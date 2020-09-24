5be4e5b21be58a3daafbf822319f894349b0d174
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-24 00:57:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,9 @@ class Triangle
   end
   
   def kind()
-    if (s1 <= 0) || (s2 <= 0) || (s3 <= 0)
+    if (@s1 <= 0) || (@s2 <= 0) || (@s3 <= 0)
       raise TriangleError
-    elsif (s1 + s2 )
+    elsif (@s1 + @s2 <= @s3) || (@s2 + @s3 <= @s1) || (@s1 + @s3 <=
+    @s2)
 end
 
